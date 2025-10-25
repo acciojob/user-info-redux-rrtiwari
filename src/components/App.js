ecb0366fafb343ab2b4c3b85e0b4c26e8b0bb1e2
Rahul Tiwari <rt699652@gmail.com>
2025-10-25 15:00:43 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { setName, setEmail } from "./UserSlice";
 
 const App = () => {
+  const { name, email } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      <h1>User Information</h1>
 
-export default App
+      <div>
+        <input
+          type="text"
+          placeholder="Enter name"
+          value={name}
+          onChange={(e) => dispatch(setName(e.target.value))}
+          id="name-input"
+        />
+      </div>
+
+      <div>
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => dispatch(setEmail(e.target.value))}
+          id="email-input"
+        />
+      </div>
+
+      <div className="output">
+        <p>Name - {name}</p>
+        <p>Email - {email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
